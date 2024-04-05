b2c08140db277ad96af28f19632bb5911eee97b6
Patrick Oppel <patrick.oppel94@gmail.com>
2024-04-05 17:24:00 +1100

    add clone to RcHK

--- src/objects.rs
+++ src/objects.rs
@@ -1,7 +1,7 @@
 use serde::*;
 
 // #[derive(Default)]
-#[derive(Debug, Serialize, Deserialize)]
+#[derive(Debug, Clone, Serialize, Deserialize)]
 pub struct RCHk {
     pub rc1_reading: i16,
     pub rc2_reading: i16,
